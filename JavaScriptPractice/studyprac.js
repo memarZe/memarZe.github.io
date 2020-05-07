4ccfe744fea9652bfd6f8b5f35f9c0294ad4147f
memarZe <61173549+memarZe@users.noreply.github.com>
2020-05-06 21:17:22 -0500

    clock

--- JavaScriptPractice/studyprac.js
+++ JavaScriptPractice/studyprac.js
@@ -1,43 +1,20 @@
-(function() {
-    "use strict";
+//code is not working for question 8 ?
+(function () {
+
+    "use restrict";
 
 
-    const sayHello = () =>{
-        return "Hello";
+    class User {
+
+    constructor(name){
+            this.name = name;
+        }
+
+        sayHi() {
+            alert(this.name);
+    }
     }
 
-    const hello = (greeter, person) => {
-        return greeter() + ' ' + person
-    }
-
-    console.log(hello(sayHello, "Memar"));
-
-    const initialValue = 0;
-
-    const sum = [{x: 1}, {x:2}, {x:3}];
-    const x = sum.reduce(
-        function (accumulator, currentValue) {return accumulator + currentValue.x;},initialValue
-    );
-
-    document.writeln(x); // logs 6
-
-    let arr = [3, 4, 5, 6];
-    let add = arr.reduce(function (accumulator, element) {
-        return  accumulator + element;
-    });
-            console.log(add);
-
-    const str = "Memar Zelealem";
-    let rever = str.split('').reduce(function (acc, ele) {
-        return ele + acc;
-    });
-    console.log(rever);
-
-    function f(x, y) {
-        console.log(x + '' + y);
-    }
-
-    f("Memar");
-
+    let user = new User("John");
+    user.sayHi();
 })();
-
