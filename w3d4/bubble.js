e0b138c97618cb00206576892dc4ee2d92678b83
memarZe <61173549+memarZe@users.noreply.github.com>
2020-05-07 23:18:44 -0500

    clear screen

--- w3d4/bubble.js
+++ w3d4/bubble.js
@@ -1,83 +1,36 @@
-$(function () {
-    let cleanScreenData = {
-        timer: null
-    };
-
-    function getValue(id) {
-        return $("#"+id).val();
-    }
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    function getRandomColor() {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[getRandomInt(16)];
+$(function(){
+    $("#start").on("click", () => {
+        let numberOfCircle = parseInt($("#numCircle").val());
+        for (let i = 0; i < numberOfCircle; i++) {
+            addCircle();
         }
-        return color;
+        $("#container").on("click", ".circle", function() {$(this).hide()});
+        $("#container").on("mouseover", ".circle", function() {$(this).css("opacity", "0.3")});
+        $("#container").on("mouseleave", ".circle", function() {$(this).css("opacity", "1")});
+    });
+
+    function addCircle() {
+        let color ="rgb(" + Math.floor(Math.random() * 255)+","
+                    + Math.floor(Math.random() * 255)+","
+                    + Math.floor(Math.random() * 255) +")";
+        let width = $("#width").val();
+        var $e = $("<div />").css("width", width + "px")
+            .css("height", width + "px")
+            .css("background-color", color)
+            .css("position", "absolute")
+            .css("border-radius", width + "px")
+            .css("top", Math.floor(Math.random() * 600) + "px")
+            .css("left", Math.floor(Math.random() * 1000) + "px");
+
+        $e.addClass("circle");
+        $("#container").append($e);
+        let rate = parseInt($("#rate").val());
+        let growAmt = $("#growAmt").val();
+        setInterval(function() {
+            let w = parseInt($e.css("width")) + parseInt(growAmt);
+            $e.css("width", w + "px")
+                .css("height", w + "px")
+                .css("border-radius", w + "px");
+        },rate);
     }
-    function mousemove(event) {
-        $(this).css(
-            "opacity", function (indx, oldVal) {
-                let newVal = oldVal - 0.01;
-                if(newVal<=0){
-                    this.remove();
-                }
-                return newVal;
-            }
-        );
-    }
-
-    function growCircle() {
-        let growthAmount = getValue("growthAmount");
-
-        function grow(idx, oldValue){
-            let newVal = (parseInt(oldValue)+parseInt(growthAmount));
-            return newVal+"px";
-        }
-
-        function move(idx, oldValue) {
-            return parseInt(oldValue)-growthAmount/2+"px";
-        }
-        $(".circle").css("width", grow);
-        $(".circle").css("height", grow);
-        $(".circle").css("top", move);
-        $(".circle").css("left", move);
-
-        $(".circle").css("border-radius", function(idx, oldValue) {
-            return parseInt(oldValue)+growthAmount/2+"px";
-        });
-    }
-
-    function getNewCircle() {
-        return $("<div>",{
-            "class": "circle",
-            "css":{
-                "background-color": getRandomColor(),
-                "top": getRandomInt(500)+"px",
-                "left": getRandomInt(1600)+"px"
-            }
-        });
-    }
-
-    function start(){
-        if(cleanScreenData.timer!=null){
-            clearInterval(cleanScreenData.timer);
-        }
-        let count = getValue("circleCount");
-        let $circles = [];
-        for(let i = 0; i < count; i++){
-            $circles.push( getNewCircle());
-        }
-        $(".circle-container").append($circles);
-        cleanScreenData.timer = setInterval(growCircle, getValue("growRate"));
-    }
-
-    $("#start").on("click", start);
-    $(".circle-container")
-        .on("click", "div.circle", function () {this.remove();})
-        .on("mousemove", "div.circle", mousemove);
 });
