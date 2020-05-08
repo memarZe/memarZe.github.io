9aff32bdd5d7d8466935a351a19e472c5eec16b7
memarZe <61173549+memarZe@users.noreply.github.com>
2020-05-07 20:36:25 -0500

    maze comments

--- w3d3/maze.js
+++ w3d3/maze.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
+    /*an object to initialize status of the game ( if the game started or not*/
 let gameStatus = {
     started: false,
 };
-
+/*an event to start the game*/
 $("#start").click(function () {
     gameStatus.started = true;
     $(".boundary").removeClass("youlose");
     $("h2").text("Good Luck!");
 
 });
-
+/*an even to control the game and if the player loses the road or violates the rule of the game, it stoped the game*/
 $(".boundary").mouseover(function () {
       if(gameStatus.started){
           $(".boundary").addClass("youlose");
@@ -18,6 +19,7 @@ $(".boundary").mouseover(function () {
       }
   });
 
+/*and event to controll the player if leaving the boundary of teh game*/
 $("#maze").mouseleave(function () {
     if(gameStatus.started){
         $(".boundary").addClass("youlose");
@@ -25,7 +27,7 @@ $("#maze").mouseleave(function () {
         $("h2").text("Sorry, you lost the Game!");
     }
 });
-
+/*an event to end the game*/
     $("#end").click(function () {
        if(gameStatus.started){
            $("h2").text("Gongratulation, you win! :) ");
